8ca3d9896bd4dc2925ba4d2d3e9c66dd62442828
Galiro <galiro@galiro.com>
2013-04-23 20:06:08 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentaDm::Application.config.secret_token = 'a779affe9f480a15c6bc77d48afb46a5cf733f83da6955ad1d3a6fbd75e9230fe5fc758440f80947d0501dd40da896b451e309acbf849d43a486329d07e184e3'
